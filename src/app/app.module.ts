d94a1afeb1d1141ca72aa35985cdde4b8090f08b
Danilo Scipioni <daniloedu@gmail.com>
2022-02-18 11:31:29 -0300

    Add components for new device and new category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DeviceComponent } from './device/device.component';
 import { CategoryComponent } from './category/category.component';
+import { NewCategoryComponent } from './new-category/new-category.component';
+import { NewDeviceComponent } from './new-device/new-device.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DeviceComponent,
-    CategoryComponent
+    CategoryComponent,
+    NewCategoryComponent,
+    NewDeviceComponent
   ],
   imports: [
     BrowserModule,
