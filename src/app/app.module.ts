6e5cb00bd640a02213e95b8fefc211969b028b55
Danilo Scipioni <daniloedu@gmail.com>
2022-02-18 10:16:43 -0300

    Created components for category and device

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DeviceComponent } from './device/device.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule
